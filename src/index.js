6b1dada3d2e9761172faa160ab00b3b38a223983
belle9410 <xinling.xu@uq.net.au>
2019-05-18 16:50:45 +1000

    handle 404

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 require('dotenv').config();
 const routes = require('./routes');
-const app = express();
-
-
+const helmet = require('helmet');
+const morgan = require('morgan')
+const notFoundHandler = require('./middlewares/notFound');
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
 app.use(routes);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => console.log(`app listen on port ${PORT}`));
